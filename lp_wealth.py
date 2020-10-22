70b32e4521669b950d4bd3b4408023a0f88fc56d
dave_white <david.alexander.white@gmail.com>
2020-10-22 16:11:35 -0700

    refactored amm sim into class for comprehensibility

--- lp_wealth.py
+++ lp_wealth.py
@@ -3,50 +3,73 @@ import numpy as np
 import brownian_bridge_extrema
 
 
-def get_standard_brownian_motion(num_samples, num_steps, step_size):
-    w = torch.randn(num_samples, num_steps) * np.sqrt(step_size)
-    return torch.cumsum(w, 1)
+class Sim:
+    def __init__(self, mu, sigma, gamma, time_step_size, num_samples):
+        self.step = 0
 
+        self.mu = mu
+        self.sigma = sigma
+        self.gamma = gamma
 
-def get_t(steps, step_size):
-    return (torch.arange(0, steps) + 1) * step_size
+        self.time_step_size = time_step_size
+        self.num_samples = num_samples
 
+        self.log_market_price = torch.zeros(self.num_samples)
+        self.log_r_alpha = torch.zeros(self.num_samples)
+        self.log_r_beta = torch.zeros(num_samples)
 
-def get_log_geometric_brownian_motion(z, s_zero, mu, sigma, step_size):
-    t = get_t(z.shape[1], step_size)
-    return torch.log(s_zero) + (mu - sigma ** 2 / 2) * t + sigma * z
+    def get_next_brownian_motion_step(self):
+        return torch.randn(self.num_samples) * np.sqrt(self.time_step_size)
 
+    def get_log_market_price_step(self, brownian_motion_step):
+        return (self.mu - self.sigma**2 / 2) * self.time_step_size + self.sigma * brownian_motion_step
 
-def get_new_log_reserves(log_reserve_tensor, gamma, log_market_price):
-    log_r_alpha = log_reserve_tensor[..., 0]
-    log_r_beta = log_reserve_tensor[..., 1]
+    def step_time(self):
 
-    log_m_u = log_r_beta - log_r_alpha
+        brownian_motion_step = self.get_next_brownian_motion_step()
 
-    log_gamma = torch.log(gamma)
+        log_r_alpha_updated, log_r_beta_updated = self.calculate_reserve_update(brownian_motion_step)
 
-    log_c_l = log_gamma + log_market_price - log_m_u
-    log_c_h = log_market_price - log_gamma - log_m_u
+        # Updates
+        self.step += 1
 
-    # AMM price too low
-    log_r_alpha_updated = torch.where(log_m_u < log_gamma + log_market_price, log_r_alpha + log_c_l * (-1 * gamma / (gamma + 1)),
-                                      log_r_alpha)
-    log_r_beta_updated = torch.where(log_m_u < log_gamma + log_market_price, log_r_beta + log_c_l * (1 / (gamma + 1)), log_r_beta)
+        self.log_r_alpha = log_r_alpha_updated
+        self.log_r_beta = log_r_beta_updated
 
-    # AMM price too high
-    log_r_alpha_updated = torch.where(log_m_u > log_market_price - log_gamma, log_r_alpha + log_c_h * (-1 / (gamma + 1)),
-                                      log_r_alpha_updated)
-    log_r_beta_updated = torch.where(log_m_u > log_market_price - log_gamma, log_r_beta + log_c_h * (gamma / (gamma + 1)),
-                                     log_r_beta_updated)
+        self.log_market_price += self.get_log_market_price_step(brownian_motion_step)
 
-    return torch.stack((log_r_alpha_updated, log_r_beta_updated), dim=-1)
+    def calculate_reserve_update(self, brownian_motion_step, use_brownian_bridge_adjustment=True):
+        log_m_u = self.log_r_beta - self.log_r_alpha
 
+        if use_brownian_bridge_adjustment:
+            # If m_u is currently above m_p, then if m_p dropped in between the last sample and now, it could have caused an arb
+            # and vice versa
+            brownian_motion_step = torch.where(
+                log_m_u > self.log_market_price,
+                brownian_bridge_extrema.get_expected_brownian_bridge_min(torch.tensor(0.), self.time_step_size, torch.tensor(0.), brownian_motion_step),
+                brownian_bridge_extrema.get_expected_brownian_bridge_max(torch.tensor(0.), self.time_step_size, torch.tensor(0.), brownian_motion_step),
+            )
 
-def get_log_reserve_tensor(gamma, log_market_price, s_zero):
-    out_tensor = torch.zeros(list(log_market_price.shape) + [2])
-    out_tensor[..., 1] = torch.log(s_zero)
-    for i in range(1, log_market_price.shape[1]):
-        if i % max(round(log_market_price.shape[1] / 10), 2) == 1:
-            print(i, log_market_price.shape[1])
-        out_tensor[:, i, :] = get_new_log_reserves(out_tensor[:, i - 1, :], gamma, log_market_price[:, i])
-    return out_tensor
+        expected_log_market_price = self.log_market_price + self.get_log_market_price_step(brownian_motion_step)
+
+        log_gamma = torch.log(self.gamma)
+
+        log_c_l = log_gamma + expected_log_market_price - log_m_u
+        log_c_h = expected_log_market_price - log_gamma - log_m_u
+
+        # AMM price too low
+        log_r_alpha_updated = torch.where(log_m_u < log_gamma + expected_log_market_price,
+                                          self.log_r_alpha + log_c_l * (-1 * self.gamma / (self.gamma + 1)),
+                                          self.log_r_alpha)
+        log_r_beta_updated = torch.where(log_m_u < log_gamma + expected_log_market_price,
+                                         self.log_r_beta + log_c_l * (1 / (self.gamma + 1)), self.log_r_beta)
+
+        # AMM price too high
+        log_r_alpha_updated = torch.where(log_m_u > expected_log_market_price - log_gamma,
+                                          self.log_r_alpha + log_c_h * (-1 / (self.gamma + 1)),
+                                          log_r_alpha_updated)
+        log_r_beta_updated = torch.where(log_m_u > expected_log_market_price - log_gamma,
+                                         self.log_r_beta + log_c_h * (self.gamma / (self.gamma + 1)),
+                                         log_r_beta_updated)
+        return log_r_alpha_updated, log_r_beta_updated
+
--- test/test_lp_wealth.py
+++ test/test_lp_wealth.py
@@ -1,38 +1,47 @@
 from lp_wealth import *
 
 
-def test_standard_brownian_motion():
-    motion = get_standard_brownian_motion(10000, 100, 0.02)
+def test_log_market_price():
+    mu = torch.tensor(3)
+    sigma = torch.tensor(2)
+    time_step_size = 1/100
+    sim = Sim(mu, sigma, torch.tensor(1.), time_step_size, 10000)
 
-    mean = torch.mean(motion[:, -1])
-    assert np.abs(mean - 0) < 0.1
+    steps = 100
+    for i in range(steps):
+        sim.step_time()
 
-    var = torch.var(motion[:, -1])
-    assert np.abs(var - 2) < 0.1
+    time = time_step_size * steps
+
+    mean_price = torch.mean(sim.log_market_price)
+    expected_mean = (mu - sigma ** 2/ 2) * time
+    assert np.abs(mean_price - expected_mean) < 0.25
+
+    price_sd = torch.std(sim.log_market_price)
+    expected_sd = np.sqrt(time * sigma ** 2)
+    assert np.abs(price_sd - expected_sd) < 0.25
 
 
-def test_get_log_reserve_tensor():
-    # Market price tensor (3 samples, 2 time steps)
-    s = torch.ones(3, 2)
-
-    # Price of alpha at t1
-    s[0, 1] = 1
-    s[1, 1] = 1/16
-    s[2, 1] = 1/16
-
+def test_calculate_reserve_changes():
     gamma = torch.tensor([1, 0.01, 0.5])
-    log_price = torch.log(s)
-    s_zero = torch.tensor(1.)
-    res = torch.exp(get_log_reserve_tensor(gamma, log_price, s_zero))[:, -1, :]
+    # mu = 1/2 and sigma = 1 so our Brownian motion will get fed
+    # directly into our log market price
+    sim = Sim(mu=torch.tensor(1/2),
+              sigma=torch.tensor(1.),
+              gamma=gamma,
+              time_step_size=1,
+              num_samples=3
+              )
 
-    # No price change for sample 1
-    assert np.allclose(res[0, :], [1, 1])
-    # Sample 2 has a price change but gamma is too high
-    assert np.allclose(res[1, :], [1, 1])
-    # Sample has price drop, gamma is 0.5, we end up buying 3 of coin alpha
-    assert np.allclose(res[2, :], [4, .5])
+    # We're going to pass in a Brownian Motion step that will send our log
+    # price to exactly where we want it
+    brownian_motion_step = torch.log(torch.tensor([1., 1/16, 1/16]))
+    log_r_alpha, log_r_beta = sim.calculate_reserve_update(brownian_motion_step, False)
+
+    # No price change in sample 1, in sample 2 gamma is too high, in sample 3 we finally do something
+    assert np.allclose(torch.exp(log_r_alpha), torch.tensor([1.,1.,4.]))
+    assert np.allclose(torch.exp(log_r_beta), torch.tensor([1.,1.,.5]))
 
 
 if __name__ == "__main__":
-    test_standard_brownian_motion()
-    test_get_log_reserve_tensor()
+    test_calculate_reserve_changes()
