fixes to allow GPU calculation

lp_wealth.py
@@ -1,32 +1,37 @@
 import torch
-import numpy as np
 import brownian_bridge_extrema
 
 
 class Sim:
-    def __init__(self, mu, sigma, gamma, time_step_size, num_samples):
-        self.step = 0
+    def __init__(self, mu, sigma, gamma, time_step_size, num_samples, cuda=False):
+        if cuda:
+            mu = mu.cuda()
+            sigma = sigma.cuda()
+            gamma = gamma.cuda()
+            time_step_size = time_step_size.cuda()
 
         self.mu = mu
         self.sigma = sigma
         self.gamma = gamma
 
         self.time_step_size = time_step_size
+        self.step = 0
         self.num_samples = num_samples
 
-        self.log_market_price = torch.zeros(self.num_samples)
-        self.log_r_alpha = torch.zeros(self.num_samples)
-        self.log_r_beta = torch.zeros(num_samples)
+        self.log_gamma = torch.log(self.gamma)
+        self.log_market_price = torch.zeros(self.num_samples, device=mu.device)
+        self.log_r_alpha = torch.zeros(self.num_samples, device=mu.device)
+        self.log_r_beta = torch.zeros(num_samples, device=mu.device)
 
         # For testing
         self.last_normal_noise = None
         self.last_expected_log_market_price = None
 
     def get_next_normal_noise(self):
-        return torch.randn(self.num_samples)
+        return torch.randn(self.num_samples, device=self.mu.device)
 
     def get_log_market_price_step(self, normal_noise):
-        return (self.mu - self.sigma**2 / 2) * self.time_step_size + self.sigma * normal_noise * np.sqrt(self.time_step_size)
+        return (self.mu - self.sigma**2 / 2) * self.time_step_size + self.sigma * normal_noise * torch.sqrt(self.time_step_size)
 
     def step_time(self, use_brownian_bridge_adjustment=True):
 
@@ -60,23 +65,21 @@ class Sim:
         self.last_normal_noise = normal_noise
         self.last_expected_log_market_price = expected_log_market_price
 
-        log_gamma = torch.log(self.gamma)
-
-        log_c_l = log_gamma + expected_log_market_price - log_m_u
-        log_c_h = expected_log_market_price - log_gamma - log_m_u
+        log_c_l = self.log_gamma + expected_log_market_price - log_m_u
+        log_c_h = expected_log_market_price - self.log_gamma - log_m_u
 
         # AMM price too low
-        log_r_alpha_updated = torch.where(log_m_u < log_gamma + expected_log_market_price,
+        log_r_alpha_updated = torch.where(log_m_u < self.log_gamma + expected_log_market_price,
                                           self.log_r_alpha + log_c_l * (-1 * self.gamma / (self.gamma + 1)),
                                           self.log_r_alpha)
-        log_r_beta_updated = torch.where(log_m_u < log_gamma + expected_log_market_price,
+        log_r_beta_updated = torch.where(log_m_u < self.log_gamma + expected_log_market_price,
                                          self.log_r_beta + log_c_l * (1 / (self.gamma + 1)), self.log_r_beta)
 
         # AMM price too high
-        log_r_alpha_updated = torch.where(log_m_u > expected_log_market_price - log_gamma,
+        log_r_alpha_updated = torch.where(log_m_u > expected_log_market_price - self.log_gamma,
                                           self.log_r_alpha + log_c_h * (-1 / (self.gamma + 1)),
                                           log_r_alpha_updated)
-        log_r_beta_updated = torch.where(log_m_u > expected_log_market_price - log_gamma,
+        log_r_beta_updated = torch.where(log_m_u > expected_log_market_price - self.log_gamma,
                                          self.log_r_beta + log_c_h * (self.gamma / (self.gamma + 1)),
                                          log_r_beta_updated)
         return log_r_alpha_updated, log_r_beta_updated

test/test_lp_wealth.py
@@ -1,11 +1,12 @@
+import numpy as np
 from lp_wealth import *
 
 
-def test_log_market_price():
+def test_log_market_price(cuda=False):
     mu = torch.tensor(3)
     sigma = torch.tensor(2)
-    time_step_size = 1/100
-    sim = Sim(mu, sigma, torch.tensor(1.), time_step_size, 10000)
+    time_step_size = torch.tensor(1/100)
+    sim = Sim(mu, sigma, torch.tensor(1.), time_step_size, 10000, cuda=cuda)
 
     steps = 100
     for i in range(steps):
@@ -15,32 +16,36 @@ def test_log_market_price():
 
     mean_price = torch.mean(sim.log_market_price)
     expected_mean = (mu - sigma ** 2/ 2) * time
-    assert np.abs(mean_price - expected_mean) < 0.25
+    assert np.abs(mean_price.cpu() - expected_mean) < 0.25
 
     price_sd = torch.std(sim.log_market_price)
     expected_sd = np.sqrt(time * sigma ** 2)
-    assert np.abs(price_sd - expected_sd) < 0.25
+    assert np.abs(price_sd.cpu() - expected_sd) < 0.25
 
 
-def test_calculate_reserve_changes():
+def test_calculate_reserve_changes(cuda=False):
     gamma = torch.tensor([1, 0.01, 0.5])
     # mu = 1/2 and sigma = 1 so our Brownian motion will get fed
     # directly into our log market price
     sim = Sim(mu=torch.tensor(1/2),
               sigma=torch.tensor(1.),
               gamma=gamma,
-              time_step_size=1,
-              num_samples=3
+              time_step_size=torch.tensor(1.),
+              num_samples=3,
+              cuda=cuda
               )
 
     # We're going to pass in a noise realization that will send our log
     # price to exactly where we want it
     fake_noise = torch.log(torch.tensor([1., 1/16, 1/16]))
+    if cuda:
+        fake_noise = fake_noise.cuda()
+
     log_r_alpha, log_r_beta = sim.calculate_reserve_update(fake_noise, False)
 
     # No price change in sample 1, in sample 2 gamma is too high, in sample 3 we finally do something
-    assert np.allclose(torch.exp(log_r_alpha), torch.tensor([1., 1., 4.]))
-    assert np.allclose(torch.exp(log_r_beta), torch.tensor([1., 1., .5]))
+    assert np.allclose(torch.exp(log_r_alpha).cpu(), torch.tensor([1., 1., 4.]))
+    assert np.allclose(torch.exp(log_r_beta).cpu(), torch.tensor([1., 1., .5]))
 
     # Applying our Brownian Bridge adjustment has big effects because our sigma and time step are both big
     # To avoid inequality figdgetyness
@@ -62,4 +67,5 @@ def test_calculate_reserve_changes():
 
 
 if __name__ == "__main__":
-    test_calculate_reserve_changes()
+    test_log_market_price(True)
+    test_calculate_reserve_changes(True)