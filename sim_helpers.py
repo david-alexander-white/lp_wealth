1b60be57c02a568ec9659cba5f2a8b348c653714
dave_white <david.alexander.white@gmail.com>
2020-10-30 11:00:09 -0700

    add mu consideration to get_max_time_step

--- sim_helpers.py
+++ sim_helpers.py
@@ -14,12 +14,22 @@ def sim_loop(sim, num_steps, bb_adjustment_type="sample", sample_style="multi"):
     return checkpoints
 
 
-def get_max_time_step(gamma, sigma):
-    return torch.log(gamma) **2 / 4 / sigma **2
+def get_max_time_step(gamma, sigma, mu):
+    # We want to make sure trading back and forth in one timestep is a 4-sigma event
+    sigma_timestep = torch.log(gamma) **2 / 4 / sigma **2
+
+    # We want to make sure that the drift in any given timestep is at most 1/5 sigma (which scales with sqrt(t))
+    # If we have timesteps were std dev is comparable to drift, we should expect to miss out on a lot of trades
+    # in the opposite direction from drift.
+    mu_timestep = sigma ** 2 / 25 / (mu-sigma**2/2)**2 if mu is not None else torch.tensor(1.e6)
+
+    return torch.min(sigma_timestep, mu_timestep)
 
 
 def get_min_time(sigma, gamma):
-    return 400 * (torch.log(gamma) / sigma)**2
+    # The standard deviation of log m_p at time t is sigma * sqrt(t)
+    # If that's 10 * log(gamma), then we should expect to see at least a couple trades
+    return (10 * torch.log(gamma) / sigma)**2
 
 
 def get_min_time_steps(sigma, gamma, time_step):
